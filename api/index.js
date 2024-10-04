0c5464adc7e4d049e70e1561bc1579addfabe097
Erick Araújo <araujoerick.dev@gmail.com>
2024-10-04 10:42:14 -0300

    :card_file_box: raw: Edita os dados da API

--- api/index.js
+++ api/index.js
@@ -1,64 +1,60 @@
 const fastify = require("fastify")();
 const cors = require("@fastify/cors");
 
-// Habilita CORS para todas as origens
 fastify.register(cors, {
 	origin: true, // Permite todas as origens
-	methods: ["GET"], // Define os métodos permitidos (GET neste caso)
+	methods: ["GET"],
 });
 
-// Exemplo de dados de tênis Air Jordan
 const tenisAirJordan = [
 	{
 		id: 1,
 		nome: "Air Jordan 1 Mid Dutch Green",
-		preco: 150,
-		categoria: "Basketball",
-		imagem: "https://imgbox.com/eCxBaABi",
+		preco: "1.199.99",
+		categoria: "Tênis Air Jordan",
+		imagem: "https://images2.imgbox.com/9e/ae/eCxBaABi_o.png",
 	},
 	{
 		id: 2,
 		nome: "Air Jordan 1 Mid Chicago Black Toe",
-		preco: 200,
-		categoria: "Basketball",
-		imagem: "https://imgbox.com/odbxvxyt",
+		preco: "1.049,00",
+		categoria: "Tênis Air Jordan",
+		imagem: "https://images2.imgbox.com/03/c5/odbxvxyt_o.png",
 	},
 	{
 		id: 3,
 		nome: "Air Jordan 1 Mid Se Lightbulb",
-		preco: 220,
-		categoria: "Basketball",
-		imagem: "https://imgbox.com/X9yP6kBe",
+		preco: "1.350,00",
+		categoria: "Tênis Air Jordan",
+		imagem: "https://images2.imgbox.com/3b/cf/X9yP6kBe_o.png",
 	},
 	{
 		id: 4,
 		nome: "Air Jordan 1 Retro High Tie Dye",
-		preco: 180,
-		categoria: "Basketball",
-		imagem: "https://imgbox.com/rTlTVW0q",
+		preco: "1.585,00",
+		categoria: "Tênis Air Jordan",
+		imagem: "https://images2.imgbox.com/10/37/rTlTVW0q_o.png",
 	},
 	{
 		id: 5,
-		nome: "Air Jordan 1 Mid Hyper Royal Tumbled Leather",
-		preco: 210,
-		categoria: "Basketball",
-		imagem: "https://imgbox.com/NI46K1WM",
+		nome: "Air Jordan 1 Mid Hyper Royal Leather",
+		preco: "949,99",
+		categoria: "Tênis Air Jordan",
+		imagem: "https://images2.imgbox.com/1f/0b/NI46K1WM_o.png",
 	},
 	{
 		id: 6,
 		nome: "Air Jordan 1 Retro High Tokyo Bio Hack",
-		preco: 190,
-		categoria: "Casual",
-		imagem: "https://imgbox.com/sNW9WzDz",
+		preco: "999,99",
+		categoria: "Tênis Air Jordan",
+		imagem: "https://images2.imgbox.com/ed/54/sNW9WzDz_o.png",
 	},
 ];
 
-// Rota que retorna os tênis Air Jordan
 fastify.get("/air-jordan", async (request, reply) => {
 	return { tenis: tenisAirJordan };
 });
 
-// Exporta uma função que lida com as requisições
 module.exports = (req, res) => {
 	fastify.ready((err) => {
 		if (err) throw err;
